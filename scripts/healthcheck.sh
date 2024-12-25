2d0e46bee0743f89499dab9547d783ea426e5e5d
Bar Abudi <barabudy@gmail.com>
2024-12-25 16:44:49 +0200

    Updated healthcheck command

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# Read the username from the secrets file
+# Read username & database name from the secrets file
 POSTGRES_USER=$(cat /run/secrets/postgres_user)
+DATABASE_NAME=${POSTGRES_DB:-smart-home-db}  # Default to "smart-home-db" if not set
 
 # Run the health check
-exec pg_isready -U "$POSTGRES_USER"
+exec pg_isready -U "$POSTGRES_USER" -d "$DATABASE_NAME"
