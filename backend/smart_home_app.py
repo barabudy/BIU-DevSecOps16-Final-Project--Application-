e1d3e885e09705f454e96343688226972d7a06fd
Bar Abudi <barabudy@gmail.com>
2025-01-04 16:31:10 +0200

    Test pipeline on dev commit 1

--- backend/smart_home_app.py
+++ backend/smart_home_app.py
@@ -5,6 +5,7 @@ from datetime import datetime # Used for date and time validation for sensors
 from dotenv import load_dotenv
 import logging
 import os
+# Test pipeline
 
 logging.basicConfig(level=logging.ERROR)
 
