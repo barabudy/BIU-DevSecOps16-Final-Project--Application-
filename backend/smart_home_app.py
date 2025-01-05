8c5ea3c3ace88c37be2b7404ec3cfe9d5cda1678
Bar Abudi <barabudy@gmail.com>
2025-01-05 13:37:43 +0200

    Test pipeline in new branch

--- backend/smart_home_app.py
+++ backend/smart_home_app.py
@@ -6,6 +6,7 @@ from datetime import datetime # Used for date and time validation for sensors
 from dotenv import load_dotenv
 import logging
 import os
+# Test CI pipeline
 
 logging.basicConfig(level=logging.ERROR)
 
